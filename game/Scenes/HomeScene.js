f2525e29cb4643316aa263e7e7b289f6d9904be0
Andrew Kreisher <akreish@mit.edu>
2024-08-08 22:44:04 -0400

    Remove start game option from home

--- game/Scenes/HomeScene.js
+++ game/Scenes/HomeScene.js
@@ -11,28 +11,12 @@ export class HomeScene extends Phaser.Scene {
     this.add.text(this.cameras.main.centerX/2 +125, 50, 'Inky', { fill: '#000' }).setFont('200px Arial');
     var background = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'background').setScale(1.2).setDepth(-2);
 
-    let startGameText = this.add.text(150, 450, 'Start Game', { font: '50px Arial', fill: '#ff0000' })
-            .setOrigin(0.5)
-            .setInteractive();
-
-    startGameText.on('pointerdown', () => {
-            this.scene.start('MainScene'); // This will switch to your main game scene
-        });
-    startGameText.on('pointerover', () => {
-        startGameText.setColor('#ff00ff');
-    });
-    startGameText.on('pointerout', () => {
-        startGameText.setColor('#ff0000');
-    });
-
-
-
     let joinGameText = this.add.text(150, 550, 'Join Game', { font: '50px Arial', fill: '#ff0000' })
             .setOrigin(0.5)
             .setInteractive();
 
     joinGameText.on('pointerdown', () => {
-            this.scene.start('LobbyScene'); // This will switch to your main game scene
+            this.scene.start('LobbyScene'); 
         });
     joinGameText.on('pointerover', () => {
         joinGameText.setColor('#ff00ff');
